3c8900f79b14211b7454607149bcd30304030118
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2021-04-06 16:40:25 -0300

    Blank base openGL structure

--- src/main/kotlin/br/com/tagview/App.kt
+++ src/main/kotlin/br/com/tagview/App.kt
@@ -1,5 +1,9 @@
 package br.com.tagview
 
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.opengl.GL
+import java.lang.RuntimeException
+
 class App() {
     companion object {
         @JvmStatic
@@ -9,6 +13,44 @@ class App() {
     }
 
     init {
-        println("x")
+        if (!GLFW.glfwInit()) {
+            throw RuntimeException("Cannot init GLFW")
+        }
+
+        val window = GLFW.glfwCreateWindow(1280, 720, "TechTalk OpenGL", 0L, 0L)
+
+        if (window == 0L) {
+            throw RuntimeException("Cannot create window")
+        }
+
+        GLFW.glfwMakeContextCurrent(window)
+        GLFW.glfwSwapInterval(1)
+
+        GL.createCapabilities()
+
+        prepare()
+
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            loop()
+            GLFW.glfwSwapBuffers(window)
+            GLFW.glfwPollEvents()
+        }
+
+        finish()
+
+        GLFW.glfwDestroyWindow(window)
+        GLFW.glfwTerminate()
+    }
+
+    private fun prepare() {
+
+    }
+
+    private fun loop() {
+
+    }
+
+    private fun finish() {
+
     }
 }

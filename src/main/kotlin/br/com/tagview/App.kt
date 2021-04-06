493a54647d3eb1283582766989527f619cee03f9
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2021-04-06 16:17:59 -0300

    Initial maven structure

---
+++ src/main/kotlin/br/com/tagview/App.kt
@@ -0,0 +1,14 @@
+package br.com.tagview
+
+class App() {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            App()
+        }
+    }
+
+    init {
+        println("x")
+    }
+}
